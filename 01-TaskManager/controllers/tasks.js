803c2d8943ef6b047becb3ab274a64040f5e857d
LongCV1 <longcv1.khmt@gmail.com>
2022-01-13 22:06:18 +0700

    Update task in controller

--- 01-TaskManager/controllers/tasks.js
+++ 01-TaskManager/controllers/tasks.js
@@ -1,12 +1,29 @@
 const Task = require('../models/Task');
 
 
-const getAllTasks = (req, res) => {
-    res.send('Get all tasks');
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        if(!tasks){
+            return res.status(404).json({msg:"Cannot get all tasks!"});
+        }
+        return res.status(200).json({msg:"All tasks found", tasks});
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 };
 
-const getTask = (req, res) => {
-    res.send('Get a single task');
+const getTask = async(req, res) => {
+    const {id:taskId} = req.params;
+    try {
+        const task = await Task.findOne({_id:taskId});
+        if(!task){
+            return res.status(404).json({msg:"Task not found!"});
+        }
+        return res.status(200).json({msg: "Task found"},task);
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 };
 
 const createTask = async (req, res) => {
