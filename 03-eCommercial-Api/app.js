8567c47e5912802fd6e6220acf0f9fd3acbf52b7
longcv1 <longcv1.khmt@gmail.com>
2022-03-14 23:14:20 +0700

    Review Controller

--- 03-eCommercial-Api/app.js
+++ 03-eCommercial-Api/app.js
@@ -1,38 +1,39 @@
 /**
  * Add Dependencies
  */
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express')
-const connectDB = require('./db/connect');
-const NotFoundHandler = require('./middleware/not-found');
-const ErrorHandler = require('./middleware/error-handler');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileUpload');
-const authRouter = require('./routes/AuthRoute');
-const userRouter = require('./routes/UserRoute');
-const productRouter = require('./routes/ProductRoute');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const connectDB = require("./db/connect");
+const NotFoundHandler = require("./middleware/not-found");
+const ErrorHandler = require("./middleware/error-handler");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileUpload");
+const authRouter = require("./routes/AuthRoute");
+const userRouter = require("./routes/UserRoute");
+const productRouter = require("./routes/ProductRoute");
+const reviewRouter = require("./routes/ReviewRoute");
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(fileUpload());
 
 /**
  * Routes
  */
-app.get('/v1/api', (req, res) => {
-    //console.log(req.signedCookies);
-    res.send('Demo App eCommercial Api');
-})
-app.use('/v1/api/auth', authRouter);
-app.use('/v1/api/users', userRouter);
-app.use('/v1/api/products', productRouter);
+app.get("/v1/api", (req, res) => {
+  res.send("Demo App eCommercial Api");
+});
+app.use("/v1/api/auth", authRouter);
+app.use("/v1/api/users", userRouter);
+app.use("/v1/api/products", productRouter);
+app.use("/v1/api/reviews", reviewRouter);
 
 /**
  * Use Middleware
@@ -43,9 +44,9 @@ app.use(ErrorHandler);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
-    await connectDB(process.env.MONGO_URL);
-    console.log('Connection DB is OK!');
-    app.listen(port, console.log(`Running at port ${port}...`));
-}
+  await connectDB(process.env.MONGO_URL);
+  console.log("Connection DB is OK!");
+  app.listen(port, console.log(`Running at port ${port}...`));
+};
 
 start();
---
+++ 03-eCommercial-Api/models/Review.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+    },
+    title: {
+      type: String,
+    },
+    comment: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Review", ReviewSchema);
